af2ef2281a1b6ccde1bd973fb4e7d627f437a83c
ElectronicMakerSpace <temperaturasensors@gmail.com>
2018-09-07 19:07:31 -0500

    Aplicacion Final

--- server/index.js
+++ server/index.js
@@ -16,6 +16,83 @@ app.get('/humidity', function (req, res) {
   })
 })
 
-app.listen(3000, function () {
-	console.log('Servidor escuchando en el puerto 3000')
+/**/
+app.get('/temperature/history', function (req, res) {
+databaseOperations.fetchLatestReadings('temperature', 10, (err, results) => {
+if (err) {
+    console.error(err)
+    return res.status(500).end()
+}
+    res.json(results.reverse())
+    })
 })
+
+app.get('/humidity/history', function (req, res) {
+databaseOperations.fetchLatestReadings('humidity', 10, (err, results) => {
+if (err) {
+console.error(err)
+return res.status(500).end()
+}
+	res.json(results.reverse())
+})
+})
+/**/
+app.get('/temperature/range', function (req, res) {
+const {start, end} = req.query
+databaseOperations.fetchReadingsBetweenTime('temperature', start, end, (err, results) => {
+if (err) {
+console.error(err)
+return res.status(500).end()
+}
+res.json(results)
+})
+})
+
+app.get('/temperature/average', function (req, res) {
+const {start, end} = req.query
+databaseOperations.getAverageOfReadingsBetweenTime('temperature', start, end, (err, results) => {
+if (err) {
+console.error(err)
+return res.status(500).end()
+}
+
+res.json({
+    value: results['avg(value)'].toFixed(1)
+    })
+
+})
+})
+/**/
+const http = require('http')
+const socketIo = require('socket.io')
+const {subscribe, unsubscribe} = require('./notifier')
+const httpServer = http.Server(app)
+const io = socketIo(httpServer)
+
+io.on('connection', socket => {
+console.log(`User connected [${socket.id}]`)
+
+const pushTemperature = newTemperature => {
+    socket.emit('new-temperature', {
+    value: newTemperature
+})
+}
+const pushHumidity = newHumidity => {
+    socket.emit('new-humidity', {
+    value: newHumidity
+})
+}
+subscribe(pushTemperature, 'temperature')
+subscribe(pushHumidity, 'humidity')
+socket.on('disconnect', () => {
+unsubscribe(pushTemperature, 'temperature')
+unsubscribe(pushHumidity, 'humidity')
+})
+})
+httpServer.listen(3000, function () {
+console.log('Server listening on port 3000')
+})
+
+// app.listen(3000, function () {
+// console.log('Server listening on port 3000')
+// })
--- server/public/script.js
+++ server/public/script.js
@@ -93,8 +93,152 @@ humidityChart.update()
 humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'
 })
 }
+/**/
+const fetchTemperatureHistory = () => {
+fetch('/temperature/history').then(results => {
+    return results.json()
+    })
+.then(data => {
+    data.forEach(reading => {
+    const time = new Date(reading.createdAt + 'Z')
+    const formattedTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+    pushData(temperatureChartConfig.data.labels,formattedTime, 10)
+    pushData(temperatureChartConfig.data.datasets[0].data, reading.value, 10)
+    })
+    temperatureChart.update()
+    })
+}
 
+/*fetchTemperatureHistory()*/
+
+const fetchHumidityHistory = () => {
+fetch('/humidity/history').then(results => {
+    return results.json()
+    })
+.then(data => {
+    data.forEach(reading => {
+        const time = new Date(reading.createdAt + 'Z')
+        const formattedTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+        pushData(humidityChartConfig.data.labels,formattedTime, 10)
+        pushData(humidityChartConfig.data.datasets[0].data,reading.value, 10)
+    })
+    humidityChart.update()
+    })
+}
+/*fetchHumidityHistory()*/
+/**/
+/*
 setInterval(() => {
     fetchTemperature()
     fetchHumidity()
 }, 2000)
+*/
+function getParameterByName (name) {
+    const url = window.location.href
+    name = name.replace(/[\[\]]/g, '\\$&')
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+    const results = regex.exec(url)
+    if (!results) return null
+    if (!results[2]) return ''
+    return decodeURIComponent(results[2].replace(/\+/g, ''))
+}
+const fetchTemperatureRange = () => {
+    const start = getParameterByName('start')
+    const end = getParameterByName('end')
+    fetch(`/temperature/range?start=${start}&end=${end}`).then(results => {
+    return results.json()
+    })
+    .then(data => {
+    data.forEach(reading => {
+
+    const time = new Date(reading.createdAt + 'Z')
+    const formattedTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+    pushData(temperatureChartConfig.data.labels,formattedTime, 10)
+    pushData(temperatureChartConfig.data.datasets[0].data,reading.value,10)
+    })
+    temperatureChart.update()
+        })
+    fetch(`/temperature/average?start=${start}&end=${end}`)
+    .then(results => {
+    return results.json()
+    })
+    .then(data => {
+    temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>'})
+}
+
+const fetchHumidityRange = () => {
+  const start = getParameterByName('start')
+  const end = getParameterByName('end')
+  fetch(`/humidity/range?start=${start}&end=${end}`).then(results => {
+      return results.json()
+    })
+    .then(data => {
+      data.forEach(reading => {
+        const time = new Date(reading.createdAt + 'Z')
+        const formattedTime =
+          time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+        pushData(humidityChartConfig.data.labels, formattedTime, 10)
+        pushData(humidityChartConfig.data.datasets[0].data, reading.value, 10)
+      })
+      humidityChart.update()
+    })
+  fetch(`/humidity/average?start=${start}&end=${end}`)
+    .then(results => {
+      return results.json()
+    })
+    .then(data => {
+      humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'})
+}
+/**
+ * First, define a function that will initialize the socket connection and add listeners
+ * to the required events
+ */
+const addSocketListeners = () => {
+  /**
+   * The "io" constructor is available to us after including the socket.io library script in the "index.html" file
+   * Initializing the socket connection is as easy as the statement below
+   */
+  const socket = io()
+
+  /**
+   * An event listener is attached to the "new-temperature" event
+   * The handler is similar to the handler that was attached to the GET /temeprature API, so in essence, we are replacing the API call with the socket event notification
+   */
+  socket.on('new-temperature', data => {
+    const now = new Date()
+    const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+    pushData(temperatureChartConfig.data.labels, timeNow, 10)
+    pushData(temperatureChartConfig.data.datasets[0].data, data.value, 10)
+
+    temperatureChart.update()
+    temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>'})
+
+  /**
+   * Similarly, we add the handler for the "new-humidity" event
+   */
+  socket.on('new-humidity', data => {
+    const now = new Date()
+    const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+    pushData(humidityChartConfig.data.labels, timeNow, 10)
+    pushData(humidityChartConfig.data.datasets[0].data, data.value, 10)
+
+    humidityChart.update()
+    humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'})
+}
+if (!getParameterByName('start') && !getParameterByName('end')) {
+  /**
+   * Finally, the fetchHumidity and fetchTemperature functions, that used to call the APIs at regular intervals, are removed.
+   * In their place, the addSocketListeners function is called (and only needs to be called once this time)
+   */
+  addSocketListeners()
+
+  // setInterval(() => {
+    // fetchTemperature()
+    // fetchHumidity()
+  // }, 2000)
+  fetchHumidityHistory()
+  fetchTemperatureHistory()
+} else {
+  fetchHumidityRange()
+  fetchTemperatureRange()
+}
